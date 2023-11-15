84117d70fc7e49926cfeacf4b36472fbd63e871c
santicano <canosantiago404@gmail.com>
2023-11-15 16:11:31 -0300

    added models

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: { type: String, required: true, minLength: 4, MaxLength: 18 },
+    firstname: { type: String, required: true, minLength: 2, maxLength: 18 },
+    lastname: { type: String, required: true, minLength: 2, maxLength: 18 },
+    password: { type: String, required: true },
+    isMember: { type: Boolean, required: true, default: false },
+    isAdmin: { type: Boolean, required: true, default: false },
+});
+
+UserSchema.virtual("fullname").get(function () {
+    return `${this.firstname} ${this.lastname}`;
+});
+
+module.exports = mongoose.model("User", UserSchema);
